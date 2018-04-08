bbd4c127157389e696d5e90d265f5a7fcc56f963
蔡雪丹 <caixuedan@corp.netease.com>
2018-04-08 19:22:20 +0800

    数据不改变时不渲染

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+实现步骤见commit历史
+
 1. 创建store、dispatch
 2. 抽离store，抛出getState和dispatch方法
+3. 监听数据变化，重新渲染视图
--- src/index.js
+++ src/index.js
@@ -18,21 +18,24 @@ const appState = {
 }
 
 // 渲染内容
-function renderApp(appState) {
-    renderTitle(appState.title)
-    renderContent(appState.content)
+function renderApp(oldState, newState) {
+    if (newState === oldState) return;
+    renderTitle(appState, newState)
+    renderContent(appState, newState)
 }
 
-function renderTitle(title) {
-    const titleDOM = document.getElementById('title')
-    titleDOM.innerHTML = title.text
-    titleDOM.style.color = title.color
+function renderTitle(oldState, newState) {
+    if (newState === oldState) return;
+    const titleDOM = document.getElementById('title');
+    titleDOM.innerHTML = newState.title.text;
+    titleDOM.style.color = newState.title.color;
 }
 
-function renderContent(content) {
-    const contentDOM = document.getElementById('content')
-    contentDOM.innerHTML = content.text
-    contentDOM.style.color = content.color
+function renderContent(oldState, newState) {
+    if (newState === oldState) return;
+    const contentDOM = document.getElementById('content');
+    contentDOM.innerHTML = newState.content.text;
+    contentDOM.style.color = newState.content.color;
 }
 
 /**
@@ -41,9 +44,14 @@ function renderContent(content) {
  * @return {getState} 返回state {dispatch} 用于修改数据
  */
 function createStore(state, stateChanger) {
+    const listeners = [];
+    const subscribe = (listener) => listeners.push(listener);
     const getState = () => state;
-    const dispatch = (action) => stateChanger(state, action);
-    return { getState, dispatch };
+    const dispatch = (action) => {
+        state = stateChanger(state, action);
+        listeners.forEach((listener) => listener()); // 每次dispatch时遍历监听者数组调用渲染函数
+    }
+    return { getState, dispatch, subscribe };
 }
 
 /**
@@ -54,20 +62,37 @@ function createStore(state, stateChanger) {
 function stateChanger(state, action) {
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            state.title.text = action.text;
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            }
             break;
         case 'UPDATE_TITLE_COLOR':
-            state.title.color = action.color;
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            }
             break;
         default:
+            return state;
             break;
     }
 }
 
-const store = createStore(appState, stateChanger)
+const store = createStore(appState, stateChanger);
+let oldState = store.getState();
+store.subscribe(() => {
+    const newState = store.getState(); // 获取新的state
+    renderApp(oldState, newState);
+    oldState = newState;
+}); // 添加监听者
 
-
-renderApp(appState);
+renderApp({}, appState); // 首次渲染
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js》' }) // 修改标题文本
 store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
-renderApp(appState);
